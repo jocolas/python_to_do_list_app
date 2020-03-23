2de14d1d74c793236927e43cebf9c4710affb8cc
Joe <weonmart12@gmail.com>
2020-03-23 02:57:30 -0300

    universidad , alumno , curso y credito

---
+++ universidad/Apps/GestionAcademica/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from universidad.Apps.GestionAcademica.models import *
+
+# Register your models here.
+
+admin.site.register(Alumno)
+admin.site.register(Curso)
+admin.site.register(Matricula)
